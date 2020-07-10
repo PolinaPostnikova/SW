0598dc1645c709b01569b447f893a9102dfeb9e6
WakaAnna <62556471+WakaAnna@users.noreply.github.com>
2020-07-10 14:57:43 +0300

    Update v_semantics_group_delt.sql

--- Views/v_semantics_group_delt.sql
+++ Views/v_semantics_group_delt.sql
@@ -1,12 +1,20 @@
-DROP VIEW IF EXISTS `seowork.svyaznoy_views.v_semantics_query_group`;
+DROP VIEW IF EXISTS `raiffeisen-owox.SEO_Ashmanov.v_semantics_group`;
 
-CREATE VIEW `seowork.svyaznoy_views.v_semantics_query_group` AS
+CREATE VIEW `raiffeisen-owox.SEO_Ashmanov.v_semantics_group` AS
 
 WITH adapter AS (
-    SELECT *, ROW_NUMBER()
-    OVER(PARTITION BY date, project_id, search_engine, query_group_id ORDER BY created_at DESC) as state
-    FROM `seowork.svyaznoy_source.semantics_query_group`
+    SELECT *, ROW_NUMBER() OVER(PARTITION BY date, project_id, search_engine, group_id ORDER BY created_at DESC) as state
+    FROM `raiffeisen-owox.SEO_Ashmanov.semantics_group`
+    WHERE queries_count IS NOT NULL
+),
+delta_adapter AS (
+  SELECT *,
+    LAG(avg_position) OVER(PARTITION BY project_id, search_engine, group_id ORDER BY date) - avg_position as avg_position_diff,
+    round((LAG(avg_position) OVER(PARTITION BY project_id, search_engine, group_id ORDER BY date) - avg_position) / avg_position * 100) as avg_position_diff_percent
+  FROM adapter
+  WHERE state = 1
 )
+
 SELECT
     project_id,
     project_name,
@@ -15,28 +23,37 @@ SELECT
     search_engine,
     region_id,
     region_name,
-    is_mobile, 
-    queries_count, 
-    documents_count, 
-    query_group_id, 
-    query_group_name,
+    is_mobile,
+    group_id,
+    group_name,
+    queries_groups_count,
+    queries_count,
+    documents_count,
+    categories_count,
     avg_position,
+    avg_position_diff,
+    avg_position_diff_percent,
     frequency1,
     frequency2,
     frequency3,
-    top3_count, 
-    top5_count, 
-    top10_count, 
-    top100_count, 
-    top3_percent, 
-    top5_percent, 
-    top10_percent, 
-    top100_percent, 
-    frequency1_top10_percent, 
-    frequency2_top10_percent, 
-    frequency1_top10_count, 
-    potential_traffic, 
-    potential_traffic_percent
-FROM adapter
+    top3_count,
+    top5_count,
+    top10_count,
+    top100_count,
+    top3_percent,
+    top5_percent,
+    top10_percent,
+    top100_percent,
+    frequency1_top10_percent,
+    frequency2_top10_percent,
+    frequency1_top10_count,
+    frequency2_top10_count,
+    potential_traffic,
+    potential_traffic_percent,
+    worth,
+    worth_max,
+    worth_reserve,
+    worth_percent
+FROM delta_adapter
 WHERE state = 1
-ORDER BY date DESC, project_id, search_engine;
+ORDER BY date DESC, project_id, search_engine, group_id;

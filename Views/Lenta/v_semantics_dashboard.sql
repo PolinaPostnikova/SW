8e7f4f60e8d7f6ff03d9b546701b4c0bb7db6314
WakaAnna <62556471+WakaAnna@users.noreply.github.com>
2020-08-18 17:16:02 +0300

    Update v_semantics_dashboard.sql

--- Views/Lenta/v_semantics_dashboard.sql
+++ Views/Lenta/v_semantics_dashboard.sql
@@ -6,21 +6,16 @@ WITH adapter AS (
     SELECT *, ROW_NUMBER()
     OVER(PARTITION BY date, project_id, search_engine ORDER BY created_at DESC) as state
     FROM `seowork.lenta_source.semantics_dashboard`
-    WHERE queries_count IS NOT NULL AND frequency2 > 0 AND queries_count > 0 AND frequency2_top10_percent > 0 AND potential_traffic_percent > 0 AND top3_percent > 0 AND top5_percent > 0
+    WHERE queries_count IS NOT NULL AND frequency2 > 0
 ),
 
 delta_adapter AS (
   SELECT *,
-    round(LAG(top3_percent) OVER(PARTITION BY project_id, search_engine ORDER BY date) - top3_percent) as top3_percent_diff,
-    round((LAG(top3_percent) OVER(PARTITION BY project_id, search_engine ORDER BY date) - top3_percent) / top3_percent * 100) as top3_percent_diff_percent,
-    round(LAG(top5_percent) OVER(PARTITION BY project_id, search_engine ORDER BY date) - top5_percent) as top5_percent_diff,
-    round((LAG(top5_percent) OVER(PARTITION BY project_id, search_engine ORDER BY date) - top5_percent) / top5_percent * 100) as top5_percent_diff_percent,
-    round(LAG(top10_percent) OVER(PARTITION BY project_id, search_engine ORDER BY date) - top10_percent) as top10_percent_diff,
-    round((LAG(top10_percent) OVER(PARTITION BY project_id, search_engine ORDER BY date) - top10_percent) / top10_percent * 100) as top10_percent_diff_percent,
-    round(LAG(frequency2_top10_percent) OVER(PARTITION BY project_id, search_engine ORDER BY date) - frequency2_top10_percent) as frequency2_top10_percent_diff,
-    round((LAG(frequency2_top10_percent) OVER(PARTITION BY project_id, search_engine ORDER BY date) - frequency2_top10_percent) / frequency2_top10_percent * 100) as frequency2_top10_percent_diff_percent,
-    round(LAG(potential_traffic_percent) OVER(PARTITION BY project_id, search_engine ORDER BY date) - potential_traffic_percent) as potential_traffic_percent_diff,
-    round((LAG(potential_traffic_percent) OVER(PARTITION BY project_id, search_engine ORDER BY date) - potential_traffic_percent) / potential_traffic_percent * 100) as potential_traffic_percent_diff_percent 
+    round(top3_percent - LAG(top3_percent) OVER(PARTITION BY project_id, search_engine ORDER BY date)) as top3_percent_diff,
+    round(top5_percent - LAG(top5_percent) OVER(PARTITION BY project_id, search_engine ORDER BY date)) as top5_percent_diff,
+    round(top10_percent - LAG(top10_percent) OVER(PARTITION BY project_id, search_engine ORDER BY date)) as top10_percent_diff,
+    round(frequency2_top10_percent - LAG(frequency2_top10_percent) OVER(PARTITION BY project_id, search_engine ORDER BY date)) as frequency2_top10_percent_diff,
+    round(potential_traffic_percent - LAG(potential_traffic_percent) OVER(PARTITION BY project_id, search_engine ORDER BY date)) as potential_traffic_percent_diff
   FROM adapter
   WHERE state = 1
 )
@@ -49,24 +44,19 @@ SELECT
     top100_count, 
     top3_percent,
     top3_percent_diff,
-    top3_percent_diff_percent,
     top5_percent,
     top5_percent_diff,
-    top5_percent_diff_percent,
     top10_percent,
     top10_percent_diff,
-    top10_percent_diff_percent,
     top100_percent,
     frequency1_top10_percent,
     frequency2_top10_percent,
     frequency2_top10_percent_diff,
-    frequency2_top10_percent_diff_percent,
     frequency1_top10_count,
     frequency2_top10_count,
     potential_traffic,
     potential_traffic_percent,
-    potential_traffic_percent_diff,
-    potential_traffic_percent_diff_percent
+    potential_traffic_percent_diff
 FROM delta_adapter
 WHERE state = 1
 ORDER BY date DESC, project_id, search_engine;

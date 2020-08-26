01c1b067df4c88a56f00323e512daa46de39ba14
WakaAnna <62556471+WakaAnna@users.noreply.github.com>
2020-08-26 19:41:20 +0300

    Update w_avg_bu_semantics_queries.sql

--- Views/Ozon/w_avg_bu_semantics_queries.sql
+++ Views/Ozon/w_avg_bu_semantics_queries.sql
@@ -3,9 +3,9 @@ DROP VIEW IF EXISTS `seowork.ozon_views.w_avg_bu_semantics_queries`;
 CREATE VIEW `seowork.ozon_views.w_avg_bu_semantics_queries` AS
 -- адаптер исходных данных, и защита от деления на 0
 WITH adapter AS (
-    SELECT sd.project_id, project_name, bu_id, bu_name, search_engine, date,
-      query_id, query_name,
-      frequency2, avg_position, avg_position, potential_traffic
+    SELECT sd.project_id, sd.project_name, bu_id, bu_name, search_engine, 
+      date, query_id, query_name,
+      frequency2, avg_position, potential_traffic
     FROM `seowork.ozon_views.v_semantics_queries` as sd
     RIGHT JOIN `seowork.ozon_views.v_bu_projects` USING (project_id)
     WHERE frequency2 > 0 
@@ -22,10 +22,10 @@ WITH adapter AS (
 
 -- по каждому набору ключей выбираем кол-во проектов
 , keys_with_projects AS (
-    SELECT bu_id, search_engine, date, count(*) as projects
+    SELECT bu_id, search_engine, query_id, date, count(*) as projects
     FROM unique_keys
-    GROUP BY bu_id, search_engine, date
-    ORDER BY projects DESC, date DESC, bu_id, search_engine
+    GROUP BY bu_id, search_engine, query_id, date
+    ORDER BY projects DESC, date DESC, bu_id, search_engine, query_id
 )
 
 -- максимальное кол-во проектов среди дат по каждому набору ключей
@@ -48,7 +48,7 @@ WITH adapter AS (
 -- подготовленные данные для усреднения среди проектов
 , prepared_data AS (
   SELECT uk.bu_id, uk.search_engine, uk.date, project_id,
-    query_id, query_name, 
+    uk.query_id, query_name, 
     bu_name, frequency2, avg_position, potential_traffic
   FROM useful_keys as uk
   RIGHT JOIN adapter USING (bu_id, search_engine, date, query_id )
@@ -56,14 +56,17 @@ WITH adapter AS (
   ORDER BY bu_id, search_engine, date
 )
 
-SELECT
-    bu_id, bu_name, date, search_engine, count(*) as query_count,
-    ROUND(  100 *   sum(avg_position) / query_count, 2) as avg_position_count,
-    sum(query_count) as sum_queries_count,
-    sum(frequency2) as sum_frequency2
-FROM prepared_data
-GROUP BY
-    bu_id, date, search_engine, bu_name
-ORDER BY
-    date DESC, bu_id, search_engine
+, avg_position_prepared AS(
+    SELECT bu_id, bu_name, date, search_engine, count(*) as query_count,
+      sum(avg_position) as avg_position_sum,
+      sum(frequency2) as sum_frequency2
+    FROM prepared_data
+    GROUP BY bu_id, date, search_engine, bu_name
+    ORDER BY date DESC, bu_id, search_engine
+)
+SELECT bu_id, bu_name, date, search_engine, sum_frequency2,
+    ROUND(avg_position_sum / query_count, 2) as avg_position_bu
+FROM avg_position_prepared
+GROUP BY bu_id, date, search_engine, bu_name, avg_position_sum, query_count, sum_frequency2
+ORDER BY date DESC, bu_id, search_engine
 ;

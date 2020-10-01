c512c0cd5d4eb6c4d75cbd74f004e6172ec5f451
WakaAnna <62556471+WakaAnna@users.noreply.github.com>
2020-10-01 11:42:02 +0300

    Update and rename v_semantics_documents_ratio.sql to r_semantics_documents.sql

--- Views/Ozon/r_semantics_documents.sql
+++ Views/Ozon/r_semantics_documents.sql
@@ -1,3 +1,7 @@
+DROP VIEW IF EXISTS `seowork.ozon_views.r_semantics_documents`;
+
+CREATE VIEW `seowork.ozon_views.r_semantics_documents` AS
+
 SELECT
 
 *,
@@ -15,47 +19,41 @@ FROM
 (SELECT
 
 *,
-ROUND(frequency2 / frequency2_top30_count * 100, 2) AS frequency2_top30_percent,
-ROUND(frequency2 / frequency2_top50_count * 100, 2) AS frequency2_top50_percent
+ROUND(frequency2_top30_count / frequency2 * 100, 2) AS frequency2_top30_percent,
+ROUND(frequency2_top50_count / frequency2 * 100, 2) AS frequency2_top50_percent
 
 
 FROM
 
 (SELECT 
-q.document_id,
-q.search_engine,
-q.date,
-q.frequency2_top30_count, 
-q.frequency2_top50_count,
-
 d.project_id,
 d.project_name,
 d.project_description,
 d.region_id,
 d.region_name,
 d.is_mobile,
+q.search_engine,
+q.date,
+q.document_id,
 d.document_name,
 d.category_id,
 d.category_name,
 d.queries_count,
 d.avg_position,
-d.frequency1,
 d.frequency2,
 d.frequency3,
 d.top3_count,
 d.top5_count,
 d.top10_count,
-d.top100_count,
 d.top3_percent,
 d.top5_percent,
 d.top10_percent,
-d.top100_percent,
-d.frequency1_top10_percent,
 d.frequency2_top10_percent,
-d.frequency1_top10_count,
 d.frequency2_top10_count,
 d.potential_traffic,
-d.potential_traffic_percent
+d.potential_traffic_percent,
+q.frequency2_top30_count, 
+q.frequency2_top50_count
 
 FROM
 
@@ -81,5 +79,3 @@ LEFT JOIN `seowork.ozon_views.v_semantics_documents` AS d USING(document_id, dat
 WHERE d.frequency2 > 0 AND frequency2_top30_count > 0 AND frequency2_top50_count > 0)
 
 WHERE frequency2_top10_count > 0))
-LIMIT 1000
-

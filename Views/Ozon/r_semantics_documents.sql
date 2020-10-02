068ce59d7a003b378d7d30679573a5bd4c4ba7f8
WakaAnna <62556471+WakaAnna@users.noreply.github.com>
2020-10-02 12:25:47 +0300

    Update r_semantics_documents.sql

--- Views/Ozon/r_semantics_documents.sql
+++ Views/Ozon/r_semantics_documents.sql
@@ -12,15 +12,9 @@ FROM
 (SELECT
 
 *,
-ROUND((LOG10(frequency2_top30_count/(frequency2_top10_count+1))*10),2) AS frequency2_top10_30_ratio
-
-FROM
-
-(SELECT
-
-*,
 ROUND(frequency2_top30_count / frequency2 * 100, 2) AS frequency2_top30_percent,
-ROUND(frequency2_top50_count / frequency2 * 100, 2) AS frequency2_top50_percent
+ROUND(frequency2_top50_count / frequency2 * 100, 2) AS frequency2_top50_percent,
+ROUND((LOG10(frequency2_top30_count+1/(frequency2_top10_count+1))*10),2) AS frequency2_top10_30_ratio
 
 
 FROM
@@ -52,30 +46,58 @@ d.frequency2_top10_percent,
 d.frequency2_top10_count,
 d.potential_traffic,
 d.potential_traffic_percent,
-q.frequency2_top30_count, 
-q.frequency2_top50_count
+IF((q.frequency2_top30 IS NULL) IS NOT FALSE, 0, SUM (q.frequency2_top30)) as frequency2_top30_count,
+IF((q.frequency2_top50 IS NULL) IS NOT FALSE, 0, SUM (q.frequency2_top50)) as frequency2_top50_count
 
 FROM
 
-(SELECT 
+(SELECT
+  document_id,
+  search_engine,
+  date,
+  avg_position,
+  CASE
+    WHEN avg_position > 10 AND avg_position < 31 
+    THEN SUM (frequency2)
+  END frequency2_top30,
 
-a.document_id,
-a.search_engine,
-a.date,
-SUM (a.frequency2) AS frequency2_top30_count,
+  CASE
+    WHEN avg_position > 30 AND avg_position < 51 
+    THEN SUM (frequency2)
+  END frequency2_top50
 
-SUM (b.frequency2) AS frequency2_top50_count
-
-
-FROM `seowork.ozon_views.v_semantics_queries` AS a
-
-LEFT JOIN `seowork.ozon_views.v_semantics_queries` AS b USING (document_id, search_engine, date)
-
-WHERE a.avg_position > 10 AND a.avg_position < 31 AND  b.avg_position > 30 AND b.avg_position < 51
-
-GROUP BY document_id, search_engine, date) AS q
+FROM `seowork.ozon_views.v_semantics_queries`
+GROUP BY document_id, search_engine, date, avg_position) AS q
 
 LEFT JOIN `seowork.ozon_views.v_semantics_documents` AS d USING(document_id, date, search_engine)
-WHERE d.frequency2 > 0 AND frequency2_top30_count > 0 AND frequency2_top50_count > 0)
+WHERE d.frequency2 > 0 
+GROUP BY 
+d.project_id,
+d.project_name,
+d.project_description,
+d.region_id,
+d.region_name,
+d.is_mobile,
+q.search_engine,
+q.date,
+q.document_id,
+d.document_name,
+d.category_id,
+d.category_name,
+d.queries_count,
+d.avg_position,
+d.frequency2,
+d.frequency3,
+d.top3_count,
+d.top5_count,
+d.top10_count,
+d.top3_percent,
+d.top5_percent,
+d.top10_percent,
+d.frequency2_top10_percent,
+d.frequency2_top10_count,
+d.potential_traffic,
+d.potential_traffic_percent,
+q.frequency2_top30,
+q.frequency2_top50))
 
-WHERE frequency2_top10_count > 0))
